5c9ae17a1d6a67fbb0ca89db412437813f25d2b1
dmahfouz <david.mahfouz@gmail.com>
2023-01-22 17:48:56 +0000

    cleaned up stage3 solution

--- Readability Score/task/src/readability/Main.java
+++ Readability Score/task/src/readability/Main.java
@@ -14,38 +14,24 @@ public class Main {
         String filePath = args[0];
         String text = readFile(filePath);
 
-        printAriSummary(text);
-
-    }
-
-    // stage 3 methods
-
-    public static void printAriSummary(String text) {
-
         int numWords = countNumberOfWords(text);
-        int numSents = countNumberOfSentences(text);
-        int numChars = countNumberOfCharacters(text);
-        double ariScore = calculateARI(text);
-        String suggestedAge = getSuggestedAgeRange(ariScore);
+        int numChars = countNumberOfChars(text);
+        int numSents = countNumberOfSents(text);
 
-        System.out.println("The text is:");
-        System.out.println(text);
+        double ariScore = calculateAriScore(numChars, numWords, numSents);
+        String ageRange = getSuggestedAgeRange(ariScore);
 
-        System.out.printf("Words: %d%n", numWords);
-        System.out.printf("Sentences: %d%n", numSents);
-        System.out.printf("Characters: %d%n", numChars);
-        System.out.printf("The score is: %.2f%n", ariScore);
+        System.out.printf("""
+                The text is:
+                %s
+                Words: %d
+                Sentences: %d
+                Characters: %d
+                The score is: %.2f
+                This text should be understood by %s year-olds
+                """, text, numWords, numSents, numChars, ariScore, ageRange);
 
-        System.out.println(suggestedAge.equals("N/A") ?
-                "Score does not exist in ARI!" :
-                String.format("This text should be understood by %s year-olds", suggestedAge));
 
-    }
-
-    public static String readInput() {
-        // Read text from user input
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextLine();
     }
 
     public static String readFile(String filePath) {
@@ -57,19 +43,42 @@ public class Main {
         }
     }
 
-    private static String readFileAsString(String filePath) throws IOException {
+    private static String readFileAsString(
+            String filePath
+    ) throws IOException {
         // Read entire file as String
         Path fp = Paths.get(filePath);
         return new String(Files.readAllBytes(fp));
     }
 
+    private static double calculateAriScore(
+            int numChars,
+            int numWords,
+            int numSents
+    ) throws ArithmeticException {
+        return 4.71 * numChars / numWords +
+                0.5 * numWords / numSents - 21.43;
+
+    }
+
+    private static int countNumberOfSents(String text) {
+        return text.split("[.!?]").length;
+    }
+
+    private static int countNumberOfWords(String text) {
+        return text.split("\\s+").length;
+    }
+
+    private static int countNumberOfChars(String text) {
+        return text.replaceAll("\\s", "").length();
+    }
 
     public static String getSuggestedAgeRange(double ari) {
         // Returns suggested age bracket based on the Automated readability
         // index (ARI) score
         int roundedARI = (int) Math.ceil(ari);
 
-        String suggestedAgeRange = switch(roundedARI) {
+        return switch(roundedARI) {
             case 1 -> "5-6";
             case 2 -> "6-7";
             case 3 -> "7-8";
@@ -86,65 +95,5 @@ public class Main {
             case 14 -> "18-22";
             default -> "N/A";
         };
-
-        return suggestedAgeRange;
-
-
     }
-
-    public static double calculateARI(String text) {
-        // Calculates Automated readability index (ARI)
-
-        double numChars = countNumberOfCharacters(text);
-        double numWords = countNumberOfWords(text);
-        double numSents = countNumberOfSentences(text);
-
-        return 4.71 * (numChars / numWords) +
-                0.5 * (numWords / numSents) - 21.43;
-    }
-
-    // stage 2 methods
-
-    public static void estimateDifficultyUsingWords(String text) {
-        int avgWordCount = calcAvgWordsPerSentence(text);
-        printDifficulty(avgWordCount, 10);
-    }
-
-    public static void estimateDifficultyUsingSymbols(String text) {
-        int charCount = countNumberOfCharacters(text);
-        printDifficulty(charCount, 100);
-    }
-
-    public static void printDifficulty(int count, int threshold) {
-        System.out.println(count <= threshold ? "EASY": "HARD");
-    }
-
-    private static int calcAvgWordsPerSentence(String text) {
-        int numWords = countNumberOfWords(text);
-        int numSentences = countNumberOfSentences(text);
-        return numWords / numSentences;
-    }
-
-    private static int countNumberOfSentences(String text) {
-        return text.split("[?!.]").length;
-    }
-
-    private static int countNumberOfWords(String text) {
-        return text.split("\\s+").length;
-    }
-
-    private static int countNumberOfCharacters(String text) {
-        int[] chars = textToChars(text, "\s");
-        return chars.length;
-    }
-
-    private static int[] textToChars(String text, String regexExclude) {
-        return textToChars(text.replaceAll(regexExclude, ""));
-    }
-
-    private static int[] textToChars(String text) {
-        return text.chars().toArray();
-    }
-
-
 }
